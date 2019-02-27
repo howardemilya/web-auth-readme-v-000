65fc9a4ba2ecca12462f593e5288c417a1a24b87
github username <github email address>
2019-02-27 15:43:47 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get '/auth', to: 'sessions#create'
   root 'searches#search'
   get '/search', to: 'searches#search'
   post '/search', to: 'searches#foursquare'
-
+  get '/friends', to: 'searches#friends'
 end
